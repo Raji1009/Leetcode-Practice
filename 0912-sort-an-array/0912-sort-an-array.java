2dbcb38bfe14beefa5e35fc409ad12bcc557cdda
Rajalakshmi R <89059861+Raji1009@users.noreply.github.com>
2026-02-04 21:28:56 +0530

    Time: 5 ms (97.41%), Space: 77.2 MB (90.36%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        quick(nums, 0, nums.length - 1);
-        return nums;
-    }
-
-    void quick(int[] a, int l, int r) {
-        if (l >= r) return;
-        int p = a[r], i = l;
-        for (int j = l; j < r; j++){
-            if (a[j] <= p) swap(a, i++, j);
+        int max = nums[0];
+        int min = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>max){
+                max = nums[i];
+            }
+            if(nums[i]<min){
+                min = nums[i];
+            }
         }
-        swap(a, i, r);
-        quick(a, l, i - 1);
-        quick(a, i + 1, r);
-    }
-
-    void swap(int[] a, int i, int j) {
-        int t = a[i]; a[i] = a[j]; a[j] = t;
+        int range = max-min+1;
+        int[] count = new int[range];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]-min]++;
+        }
+        for(int i=1;i<range;i++){
+            count[i]+=count[i-1];
+        }
+        int[] output = new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            output[count[nums[i]-min]-1] = nums[i];
+            count[nums[i]-min]--;
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i] = output[i];
+        }
+        return nums;
     }
 }
