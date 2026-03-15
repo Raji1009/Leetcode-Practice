e9ac092a4d43f65dc8da49f5944160712b55a4fb
Rajalakshmi R <89059861+Raji1009@users.noreply.github.com>
2026-03-15 16:10:21 +0530

    Time: 2 ms (61.54%), Space: 41.9 MB (91.88%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    static int count=0;
+    public int totalNQueens(int n) {
+        count=0;
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        NQueens(board,0);
+        return count;
+    }
+    public boolean isSafe(char[][] board,int row,int col){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void NQueens(char[][] board,int row){
+        if(row==board.length){
+            count++;
+            return;
+        }
+        for(int i=0;i<board.length;i++){
+            if(isSafe(board,row,i)){
+                board[row][i]='Q';
+                NQueens(board,row+1);
+                board[row][i]='.';
+            }
+        }
+    }
+}
