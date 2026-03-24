03c543c9753a1f37ec7178f2434335f9e87465c1
Rajalakshmi R <89059861+Raji1009@users.noreply.github.com>
2026-03-24 11:21:34 +0530

    Time: 0 ms (100%), Space: 41.7 MB (98.24%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,7 +1,6 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int sum=numBottles;
-        return rec(numBottles,numExchange,sum);
+        return rec(numBottles,numExchange,numBottles);
     }
 
     public int rec(int full,int exchange,int sum){
@@ -9,8 +8,7 @@ class Solution {
             return sum;
         }
         int newbottle=full/exchange;
-        sum+=newbottle;
-        int remaingbottle=full%exchange;
-        return rec(newbottle+remaingbottle,exchange,sum);
+        int rembottle=full%exchange;
+        return rec(newbottle+rembottle,exchange,sum+newbottle);
     }
 }
