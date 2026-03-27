68d46ca0a38c278282188e8983dfe979435dd2cb
Rajalakshmi R <89059861+Raji1009@users.noreply.github.com>
2026-03-27 09:31:30 +0530

    Time: 3 ms (100%), Space: 77.3 MB (38.27%) - LeetHub

--- 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.java
+++ 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.java
@@ -1,32 +1,17 @@
 class Solution {
     public int bestRotation(int[] nums) {
         int n = nums.length;
-        int[] change = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int badStart = (i - nums[i] + 1 + n) % n;
-            int badEnd = (i + 1) % n;
-
-            change[badStart]--;
-            change[badEnd]++;
-
-            if (badStart > badEnd) {
-                change[0]--;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[(i - nums[i] + 1 + n) % n]--;
+        int current = 0, max = Integer.MIN_VALUE;
+        int index = -1;
+        for(int i = 0; i < n; i++) {
+            current += arr[i] + 1;
+            if(current > max) {
+                max = current;
+                index = i;
             }
         }
-
-        int bestK = 0;
-        int score = 0;
-        int maxScore = Integer.MIN_VALUE;
-
-        for (int k = 0; k < n; k++) {
-            score += change[k];
-            if (score > maxScore) {
-                maxScore = score;
-                bestK = k;
-            }
-        }
-
-        return bestK;
+        return index;
     }
 }
