534034b85c9c6c0f554fe3d6f23c6bfa2a15d64d
Rajalakshmi R <89059861+Raji1009@users.noreply.github.com>
2026-04-04 11:09:17 +0530

    Time: 3 ms (34.36%), Space: 45.4 MB (93.66%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,22 @@
 class Solution {
-   public List<List<Integer>> combinationSum(int[] nums, int target) {
-       Arrays.sort(nums); // Enables pruning
-       List<List<Integer>> result = new ArrayList<>();
-       List<Integer> curr = new ArrayList<>();
-       backtrack(nums, target, 0, curr, result);
-       return result;
-   }
-
-
-   private void backtrack(int[] nums, int target, int start, List<Integer> curr, List<List<Integer>> result) {
-       if (target == 0) {
-           result.add(new ArrayList<>(curr)); // copy the current combination
-           return;
-       }
-
-
-       for (int i = start; i < nums.length; i++) {
-           if (nums[i] > target) break; // Prune the path
-
-
-           curr.add(nums[i]);
-           backtrack(nums, target - nums[i], i, curr, result); // not i+1 because we can reuse the same element
-           curr.remove(curr.size() - 1); // Backtrack
-       }
-   }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList();
+        Arrays.sort(candidates);
+        reccur(candidates,target,0,ans,new ArrayList<>());
+        return ans;
+    }
+    public void reccur(int[] candidates,int target,int idx,List<List<Integer>> ans,List<Integer> curr){
+        if(target==0){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i=idx;i<candidates.length;i++){
+            if(candidates[i]>target){
+                break;
+            }
+            curr.add(candidates[i]);
+            reccur(candidates,target-candidates[i],i,ans,curr);
+            curr.remove(curr.size()-1);
+        }
+    }
 }
