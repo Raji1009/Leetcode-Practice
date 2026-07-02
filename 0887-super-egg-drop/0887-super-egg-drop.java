d6ecde64e9e5bfcdaad16768901f5aca71dd9b95
Rajalakshmi R <89059861+Raji1009@users.noreply.github.com>
2026-07-02 11:19:13 +0530

    Time: 9 ms (27.82%), Space: 47 MB (27.98%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.java
+++ 0887-super-egg-drop/0887-super-egg-drop.java
@@ -1,11 +1,11 @@
 class Solution {
     public int superEggDrop(int k, int n) {
         int moves=0;
-        int[] dp=new int[k+1];
-        while(dp[k]<n){
+        int[][] dp=new int[n+1][k+1];
+        while(dp[moves][k]<n){
             moves++;
-            for(int i=k;i>=1;i--){
-                dp[i]=dp[i]+dp[i-1]+1;
+            for(int i=1;i<=k;i++){
+                dp[moves][i]=dp[moves-1][i]+dp[moves-1][i-1]+1;
             }
         }
         return moves;
